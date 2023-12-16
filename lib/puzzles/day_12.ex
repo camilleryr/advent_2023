11da81eecd420fba99ff5299d874c3613f6876ab
Christopher Miller <32465602+camilleryr@users.noreply.github.com>
2023-12-15 23:33:39 -0600

    10x optimization for day 12

--- lib/puzzles/day_12.ex
+++ lib/puzzles/day_12.ex
@@ -38,14 +38,17 @@ defmodule Day12 do
     |> Map.get(:count)
   end
 
-  defp find([], :empty, [], state), do: Map.update!(state, :count, &(&1 + 1))
-  defp find([], 0, [], state), do: Map.update!(state, :count, &(&1 + 1))
+  defp find(rem, :empty, [], state),
+    do: if("#" in rem, do: state, else: %{state | count: state.count + 1})
+
+  defp find([], :empty, [], state), do: %{state | count: state.count + 1}
+  defp find([], 0, [], state), do: %{state | count: state.count + 1}
 
   defp find(["." | rest], :empty, groups, state), do: find(rest, :empty, groups, state)
   defp find(["." | rest], 0, groups, state), do: find(rest, :empty, groups, state)
   defp find(["." | _rest], _, _, state), do: state
 
-  defp find(["#" | rest], :empty, [], state), do: state
+  defp find(["#" | _rest], :empty, [], state), do: state
   defp find(["#" | rest], :empty, [next | groups], state), do: find(rest, next - 1, groups, state)
   defp find(["#" | _rest], 0, _, state), do: state
   defp find(["#" | rest], rem, groups, state), do: find(rest, rem - 1, groups, state)
@@ -54,24 +57,28 @@ defmodule Day12 do
     state1 = memo_find(["." | rest], rem, groups, state)
     state2 = memo_find(["#" | rest], rem, groups, %{state | memo: state1.memo})
 
-    %{count: state1.count + state2.count, memo: Map.merge(state1.memo, state2.memo)}
+    %{count: state1.count + state2.count, memo: state2.memo}
   end
 
   defp find(_, _, _, state), do: state
 
   defp memo_find(record, rem, groups, state) do
-    case Map.get(state.memo, {record, rem, groups}) do
-      nil ->
-        record
-        |> find(rem, groups, state)
-        |> then(fn ret ->
-          Map.update!(ret, :memo, &Map.put(&1, {record, rem, groups}, ret.count))
-        end)
+    key = hash({record, rem, groups})
 
-      count ->
-        Map.update!(state, :count, &(&1 + count))
+    if Map.has_key?(state.memo, key) do
+      %{state | count: state.count + Map.get(state.memo, key)}
+    else
+      record
+      |> find(rem, groups, state)
+      |> then(&update_memo(key, &1))
     end
   end
+
+  defp update_memo(key, return) do
+    %{return | memo: Map.put(return.memo, key, return.count)}
+  end
+
+  defp hash(val), do: :erlang.phash2(val, 1_000_000_000)
 
   @doc ~S"""
   ## Example
